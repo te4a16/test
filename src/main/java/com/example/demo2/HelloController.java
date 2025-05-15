839a3e98678d47dcbd3bc3f9aec68796cd033b6d
te4a16 <te220397@gmail.com>
2025-05-15 16:07:19 +0900

    自宅環境テスト

---
+++ src/main/java/com/example/demo2/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo2;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Hello world!
+ */
+@RestController
+public class HelloController {
+ 
+    @RequestMapping("/")
+    public String index(){
+        return "Hello,SpringBoot!";
+    }
+    @RequestMapping("/miyagi")
+    public String miyagi(){
+        return "Miyagi desu!";
+    }
+}
